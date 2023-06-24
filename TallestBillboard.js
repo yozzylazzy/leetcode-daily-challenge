8297f8a019ea0e4e2250c6c3caf9204a612529d5
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2023-06-24 11:43:53 +0700

    Dont have an idea

--- TallestBillboard.js
+++ TallestBillboard.js
@@ -6,6 +6,7 @@ var tallestBillboard = function (rods) {
     const x = rods.length
     let total = rods.reduce((sum, rod) => sum + rod, 0)
     let temp = 0
+    let valid = false
     for (let i = 0; i < x; i++) {
         if (total % 2 !== 0 && x > 2 && rods[i] !== rods[i + 1]) {
             total -= rods[i]
@@ -16,14 +17,15 @@ var tallestBillboard = function (rods) {
     for (let i = 0; i < x; i++) {
         temp += rods[i]
         if (temp === limiter) {
+            valid = true
             break
         }
     }
 
     console.log(temp + ' - ' + limiter)
-    if (total % 2 !== 0 || temp !== limiter) {
+    if (total % 2 !== 0) {
         return 0
     }
     return total / 2
 
-};
+}
