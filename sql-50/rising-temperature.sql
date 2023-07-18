d4c9cd7f3d6b32ca686f6834d3f9ebbb8491502e
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2023-07-18 09:29:02 +0700

    Revising rising temperature with DateDiff

--- sql-50/rising-temperature.sql
+++ sql-50/rising-temperature.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT a.id FROM weather a
-WHERE temperature > (SELECT b.temperature FROM weather b
-WHERE b.recordDate=a.recordDate-1)
+SELECT a.id FROM weather a, weather b
+WHERE DATEDIFF(a.recordDate,b.recordDate) = 1
+AND a.temperature> b.temperature
