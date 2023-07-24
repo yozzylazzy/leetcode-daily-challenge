f0f6adb4d062f389294bc4e61d45eb2bad80b90a
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2023-07-24 10:12:04 +0700

    Update answer 2 question sql 50

---
+++ sql-50/percentage-users-attended-contest.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT contest_id, ROUND(COUNT(DISTINCT r.user_id)*100/(SELECT COUNT( user_id )FROM Users ),2) as percentage
+FROM register r
+GROUP BY contest_id
+ORDER BY percentage desc, contest_id
